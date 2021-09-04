8010a6016e82b34197451c36037914a9b4c06e01
W. B. Mook <jeffrey.a.day@gmail.com>
2021-09-04 15:44:09 -0700

    Some random changes

--- _data/site.js
+++ _data/site.js
@@ -1,20 +1,20 @@
 module.exports = {
   meta: {
-    title: "My Purple Blog",
+    title: "A Template Is Better Than Nothing",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     lang: "en",
-    siteUrl: "https://example.com/",
+    siteUrl: "https://words.by86400.today/",
   },
   feed: { // used in feed.xml.njk
     subtitle: "Lorem ipsum dolor sit amet consecuteor",
     filename: "atom.xml",
     path: "/atom.xml",
-    id: "https://example.com/",
-    authorName: "John Doe",
+    id: "https://words.by86400.today/",
+    authorName: "86400",
     authorEmail: "johndoe@example.com"
   },
   hero: { // used in hero section of main page ie. index.html.njk
-    title: "Welcome to my purple blog",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusantium deserunt odio esse."
+    title: "Project notes, essays, and other bad ideas",
+    description: "Mostly, I can't fit my thoughts into 280 characters, so I should have tried this much sooner."
   }
 }
